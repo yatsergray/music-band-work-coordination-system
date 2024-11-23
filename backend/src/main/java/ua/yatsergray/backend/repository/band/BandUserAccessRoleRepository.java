25d311536d97a8935aef9d0d30d36a6002c0239e
Yezhyck <sergeysergeevytch@gmail.com>
2024-11-23 15:38:52 +0200

    Remove unused methods from repositories

--- backend/src/main/java/ua/yatsergray/backend/repository/band/BandUserAccessRoleRepository.java
+++ backend/src/main/java/ua/yatsergray/backend/repository/band/BandUserAccessRoleRepository.java
@@ -1,18 +1,13 @@
 package ua.yatsergray.backend.repository.band;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ua.yatsergray.backend.domain.entity.band.BandUserAccessRole;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BandUserAccessRoleRepository extends JpaRepository<BandUserAccessRole, UUID> {
-
-    boolean existsByBandAccessRoleId(UUID bandAccessRoleId);
 
     boolean existsByBandIdAndUserId(UUID bandId, UUID userId);
 
--- backend/src/main/java/ua/yatsergray/backend/repository/band/EventBandSongVersionRepository.java
+++ backend/src/main/java/ua/yatsergray/backend/repository/band/EventBandSongVersionRepository.java
@@ -2,10 +2,8 @@ package ua.yatsergray.backend.repository.band;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ua.yatsergray.backend.domain.entity.band.Event;
 import ua.yatsergray.backend.domain.entity.band.EventBandSongVersion;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
